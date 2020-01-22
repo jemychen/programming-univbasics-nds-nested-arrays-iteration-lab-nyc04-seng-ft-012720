3d086f9fbe5a68c9a2e7ca1df4e6af8a5cb6fc07
Jemy Chen <jemychen@live.com>
2020-01-22 03:49:50 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -41,7 +41,11 @@ def total_even_pairs(src)
   # the number was even. Review the operator documentation if you've forgotten
   # this!
   row_index = 0
-  numbers = []
+  total = 0
   while row_index < src.count do
-    if src[row_index][0]
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+      total += (src[row_index][0] + src[row_index][1])
+    end
+    row_index += 1
+  end
 end
